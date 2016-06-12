ea2c49c617fe3679a983f7f8087ae565f63f5888
Erith Margolis <dischordiandesigns@gmail.com>
2016-06-11 18:19:24 -0700

    Animated controller rough draft Animation transitions still need to be timed lower

---
+++ Tropetopia/Assets/Scripts/PlayerAnimationCalls.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationCalls : MonoBehaviour
+{
+    bool idle = false, run = false, jump = false, crouch = false;
+
+    public void OnIdle(){ idle = true; run = false; jump = false; crouch = false;}
+
+    public void OnRun() { idle = false; run = true; jump = false; crouch = false; }
+
+    public void OnJump() { idle = false; run = false; jump = true; crouch = false; }
+
+    public void OnCrouch() { idle = false; run = false; jump = false; crouch = true; }
+}
--- Tropetopia/Assets/Scripts/PlayerController.cs
+++ Tropetopia/Assets/Scripts/PlayerController.cs
@@ -3,27 +3,126 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-    PlayerAnimator animator;
+    private bool onGround;
+
+    public int health, speed, damage;
+    public float jumpHeight;
+
+    enum states {IDLE, RUN, JUMP, CROUCH};
+    states currentState;
+
+    public GameObject playerAnimator;
+    Animator anim;
+    Rigidbody2D rb;
 	// Use this for initialization
-    void Awake ()
-    {
-        animator = gameObject.GetComponent<PlayerAnimator>();
-    }
 	void Start ()
     {
-        if (animator != null)
-            StartCoroutine(animator.Anim(animator.idle));
-	}	
+        currentState = states.IDLE;
+        anim = playerAnimator.GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
 	// Update is called once per frame
 	void Update ()
     {
-	    if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.D))
         {
-            StopCoroutine(animator.Anim(animator.idle));
+            gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            transform.position += Vector3.right * speed * Time.deltaTime;
+            if (onGround)
+                currentState = states.RUN;
+            else
+                currentState = states.JUMP;
         }
-        if(Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.A))
         {
-            StartCoroutine(animator.Anim(animator.idle));
+            gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            transform.position += Vector3.left * speed * Time.deltaTime;
+            if (onGround)
+                currentState = states.RUN;
+            else
+                currentState = states.JUMP;
         }
-	}
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (onGround)
+                currentState = states.CROUCH;
+        }
+        else
+        {
+            if (onGround)
+                currentState = states.IDLE;
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            //gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            // transform.position += Vector3.up * speed * Time.deltaTime;
+            if (onGround && currentState != states.JUMP)
+            {
+                currentState = states.JUMP;
+                rb.AddForce(jumpHeight * Vector3.up);
+            }
+           
+        }
+       
+    }
+    void FixedUpdate()
+    {
+        CheckGround();
+        StateCheck();
+    }
+    void CheckGround()
+    {
+        int layermask = 1 << 8;
+        layermask = ~layermask;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 1f, layermask);
+        if (hit)
+        {
+            onGround = true;
+        }
+        else
+            onGround = false;
+        Debug.Log(onGround);
+    }
+    void StateCheck()
+    {
+        if (currentState == states.CROUCH)
+            OnCrouch();
+        else if (currentState == states.IDLE)
+            OnIdle();
+        else if (currentState == states.JUMP)
+            OnJump();
+        else if (currentState == states.RUN)
+            OnRun();
+    }
+    public void OnIdle() { anim.SetBool("idle", true);
+        anim.SetBool("run", false);
+        anim.SetBool("jump", false);
+        anim.SetBool("crouch", false); }
+
+    public void OnRun()
+    {
+        anim.SetBool("idle", false);
+        anim.SetBool("run", true);
+        anim.SetBool("jump", false);
+        anim.SetBool("crouch", false);
+    }
+
+    public void OnJump()
+    {
+        anim.SetBool("idle", false);
+        anim.SetBool("run", false);
+        anim.SetBool("jump", true);
+        anim.SetBool("crouch", false);
+    }
+
+    public void OnCrouch()
+    {
+        anim.SetBool("idle", false);
+        anim.SetBool("run", false);
+        anim.SetBool("jump", false);
+        anim.SetBool("crouch", true);
+    }
+
 }
